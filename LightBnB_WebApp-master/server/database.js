0873a1839e9f5a4b7d3c8fd45247d7820602d72b
Nick Dellar <ndelfern@live.ca>
2022-11-05 23:12:44 +0000

    change addProperties to use postgres into database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -77,9 +77,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
+
+
 
 /// Properties
 
@@ -90,17 +107,68 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+ const getAllProperties = function (options, limit = 10) {
+  
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  //Function that inserts "AND" into the query
+  const addAnd = function() {
+    if(queryParams.length) {
+      queryString += `AND `
+    }
+  }
+  
+  if (Object.keys(options).length = 1) {
+    queryString += `WHERE ` 
+  }
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `city LIKE $${queryParams.length} `;
+  } 
+
+  if (options.owner_id) {
+    addAnd()
+    queryParams.push(options.owner_id);
+    queryString += `owner_id = $${queryParams.length} `;
+  } 
+  if (options.minimum_price_per_night) {
+    addAnd()
+    queryParams.push(options.minimum_price_per_night); 
+    queryString += `cost_per_night > $${queryParams.length} `;
+  }
+  if (options.maximum_price_per_night) {
+    addAnd()
+    queryParams.push(options.maximum_price_per_night);
+    queryString += `cost_per_night < $${queryParams.length} `;
+  } 
+  
+  let realRating;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    realRating = `HAVING avg(property_reviews.rating) > $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ${realRating}
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
+
+
 
 
 /**
@@ -109,9 +177,19 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property)
+  return pool
+    .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, TRUE) RETURNING *;`
+    , [property.owner_id, property.title, property.description, property.thumbnail_photo_url, 
+      property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, 
+      property.country, property.street, property.city, property.province, property.post_code])
+    .then((property) => {
+      console.log(property.rows[0]);
+      return Promise.resolve(property.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addProperty = addProperty;
